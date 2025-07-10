c8c478045c47f78d2fc789859f88be1cb235ed91
clodon2 <90060114+clodon2@users.noreply.github.com>
2025-07-10 16:58:31 -0400

    pathing, renaming, torch

--- create_graph.py
+++ create_graph.py
@@ -1,9 +1,8 @@
-import torch_geometric as tg
 import networkx as nx
 from pandas import DataFrame
 
 
-def initialize_graphs(dataset_info:dict[str:DataFrame]):
+def create_ego_graphs(dataset_info:dict[str:DataFrame]):
     # load stuff here
     G = nx.Graph()
     for patient in dataset_info["icu_stays"]["subject_id"]:
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import load_dataset
 import create_graph
 
 
-dataset_file_path = ""
+dataset_file_path = "./mimic-iv/"
 
 
 if __name__ == "__main__":
     data_dict = load_dataset.load_dataset_files(dataset_file_path)
-    ego_graphs = create_graph.initialize_graphs(data_dict)
+    ego_graphs = create_graph.create_ego_graphs(data_dict)
